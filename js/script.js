fb5562a0af79924b4e67a64edf7756e1f515a324
lotus-pocus <116104241+lotus-pocus@users.noreply.github.com>
2022-12-14 20:49:59 +0000

    initial

---
+++ js/script.js
@@ -0,0 +1,103 @@
+var searchInput = document.querySelector(".search");
+var cardWrapper = document.querySelector("main");
+
+function noMatch() {
+  cardWrapper.innerHTML = '<p class="no-search">No results found.<p>';  
+}
+
+
+function displayMatches(matches) {
+    cardWrapper.innerHTML = '';
+
+    if (!matches.length) {
+       noMatch();
+    }
+
+
+    for (var matchObj of matches) {
+        cardWrapper.insertAdjacentHTML('beforeend', `
+      <div class="movie-card" style="background-image: 
+      linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)),  
+      url(${matchObj.movie_image})";>
+        <h3>${matchObj.title}</h3>
+        <p>${matchObj.description}</p>
+        <a href="#">${matchObj.imdb_link} target='_blank'</a>
+      </div> 
+    `)
+    }
+}
+
+
+function fetchMovies(event) {
+    var keyCode = event.keyCode;
+    var searchText = searchInput.value.toLowerCase().trim();
+
+
+
+    if (keyCode === 13 && searchText) {
+        var matches = [];
+
+        for (var movieObj of movieData) {
+            if (movieObj.title.toLowerCase().includes(searchText)) {
+                matches.push(movieObj);
+            }
+        }
+        searchInput.value = '';
+        displayMatches(matches);
+
+        fetch('https://www.omdbapi.com/?apikey=70ec0d2a&t=jurassic%20park').then(function(responseObj)) {
+            var dataPromise = responseObj.json();
+
+            dataPromise.then(function (data)) {
+                console.log(data);
+            }
+        }
+    }
+
+}
+
+function init() {
+    searchInput.addEventListener('keydown', fetchMovies);
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
